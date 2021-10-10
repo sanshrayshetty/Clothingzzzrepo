4ce03621b875428f1219e7141b61360bad104fd4
sanshrayshetty <84633439+sanshrayshetty@users.noreply.github.com>
2021-10-10 21:43:16 +0530

    sanshray

---
+++ ekart/app/src/main/java/com/Clothingzzz/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.Clothingzzz.shopping.Prevalent;
+
+import com.Clothingzzz.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
